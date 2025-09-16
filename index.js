f06cf43f92ed8869a52cbc5cf6c2cb4093bd4bce
Kutman02 <kutmank9@gmail.com>
2025-09-16 22:01:07 +0600

    Refactor movie data import to use file system methods for better path handling

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import movies from './movies.json';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const moviesFilePath = path.join(__dirname, 'movies.json');
+const movies = JSON.parse(readFileSync(moviesFilePath, 'utf8'));
 
 export default function handler(req, res) {
   let data = movies;
